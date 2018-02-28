9d04c51dacd79c25ad03f2c91cbffad63fb42230
liguoliang777 <157308001@qq.com>
2018-02-28 16:02:11 +0800

    1.成功增加Pad工程

---
+++ Pad/src/main/java/com/ngame/Utils/KeyConstantPad.java
@@ -0,0 +1,10 @@
+package com.ngame.Utils;
+
+/**
+ * Created by Administrator on 2018/2/28.
+ */
+
+public class KeyConstantPad {
+    public static final String PadService_BlueTooth_Action_Connect = "com.ngds.pad.server.PadService.Connect";
+    public static final String PadService_BlueTooth_Action_Disconnect = "com.ngds.pad.server.PadService.Disconnect";
+}
