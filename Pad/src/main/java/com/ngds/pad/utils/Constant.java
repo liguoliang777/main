238de49fbf11a02d396176f2129f1885ceb47360
liguoliang777 <157308001@qq.com>
2018-03-29 17:31:25 +0800

    1.根据游戏包名保存映射信息

--- Pad/src/main/java/com/ngds/pad/utils/Constant.java
+++ Pad/src/main/java/com/ngds/pad/utils/Constant.java
@@ -5,7 +5,7 @@ package com.ngds.pad.utils;
  */
 
 public class Constant {
-    public static int radius_Left_Stick = 120;
+    public static int radius_Left_Stick = 160;
     public static String NAME_FANGLE_PAD_SP = "name_fangle_pad_sp";
     public static String KEY_PKG_NAME_VALUE_FANGLE_PAD_SP = "key_pkg_name_value_fangle_pad_sp";
     public static String DEF_VALUE_PKG_NAME_FANGLE_PAD_SP = "def_value_fangle_pad_sp";
