ae806306c831325c97b7570426d8875a583dae75
liguoliang777 <157308001@qq.com>
2018-05-10 16:10:03 +0800

    1.界面完成

--- store/src/main/java/cn/ngame/store/activity/rank/Rank012345Fragment.java
+++ store/src/main/java/cn/ngame/store/activity/rank/Rank012345Fragment.java
@@ -333,10 +333,10 @@ public class Rank012345Fragment extends BaseSearchFragment {
             codeBtn.setTextColor(ContextCompat.getColorStateList(content, R.color
                     .fragment_rank01234_top_text_color));
             codeBtn.setButtonDrawable(new ColorDrawable(Color.TRANSPARENT));
-            codeBtn.setTextSize(15);
+            codeBtn.setTextSize(14.8f);
             codeBtn.setId(position);
             codeBtn.setText(tab2StringArr[position]);
-            codeBtn.setPadding(24, 10, 24, 15);
+            codeBtn.setPadding(25, 10, 25, 15);
 
             codeBtn.setGravity(Gravity.CENTER);
             final int finalPosition = position;
@@ -362,7 +362,7 @@ public class Rank012345Fragment extends BaseSearchFragment {
 
             LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) codeBtn
                     .getLayoutParams();
-            layoutParams.setMargins(38, 40, 0, 0);//4个参数按顺序分别是左上右下
+            layoutParams.setMargins(40, 40, 0, 0);//4个参数按顺序分别是左上右下
             codeBtn.setLayoutParams(layoutParams);
         }
     }
